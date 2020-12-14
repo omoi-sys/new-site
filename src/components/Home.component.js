bee21beaacbc09cd98ada447b20999e2724c218d
serratab <serratoa186@gmail.com>
2020-12-13 17:07:51 -0800

    fixed some styling

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -4,13 +4,12 @@ export default class About extends Component {
   render() {
     return (
     <div>
-      <h1>Welcome</h1>
-      <img className='welcome-img' src='milky-way-67504_1920.jpg' alt='milky-way' />
+      <div className='welcome-cont'>
+        <img className='welcome-img' src='milky-way-67504_1920.jpg' alt='milky-way' />
+        <div className='welcome-head'>Welcome</div>
+      </div>
       <p className='para'>
-        This is a personal website I made using ReactJS. It makes use of React Router DOM for linking to other parts of the website and navigation routing. It is a very simple personal website to practicing React.
-      </p>
-      <p className='para'>
-        Incidentally the About page has information about me, so to be sure to click above to read more.
+        This is a personal website I made using ReactJS. It makes use of React Router DOM for linking to other parts of the website and navigation routing. It is a very simple personal website to practicing React and other technologies.
       </p>
     </div>
     );
