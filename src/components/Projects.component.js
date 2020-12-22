1da64fdba884bf0e09d1ea1e185c02ad2a986fb5
omoi-sys <serratoa186@gmail.com>
2020-12-21 17:34:44 -0800

    file reformating

--- src/components/Projects.component.js
+++ src/components/Projects.component.js
@@ -7,7 +7,18 @@ export default class Projects extends Component {
         This page is to describe projects done throughout my time in the CS program.
         <dl>
           <dt><a href='https://omoi-sys.github.io/personal-old/' target='_blank'>Portfolio Website</a></dt>
-          <dd>- A simple static front-end personal website that utilizes HTML, CSS, and JavaScript. While most of the website is just made up of HTML and CSS, one section (Weather) uses JavaScript and AJAX calls to request information from the Open Weather Map API about the weather in the inputed city. This section was added both to implement a requirement to add a scroll box in one page. Rather than just add simple text,  I decided to re-implement a previous API activity on the side as well and simply showcase the JS code for those AJAX calls. By utilizing AJAX there's also no need to refresh to do more queries.</dd>
+          <dd>- A simple static front-end personal website that 
+            utilizes HTML, CSS, and JavaScript. While most of 
+            the website is just made up of HTML and CSS, one 
+            section (Weather) uses JavaScript and AJAX calls to 
+            request information from the Open Weather Map API 
+            about the weather in the inputed city. This section 
+            was added both to implement a requirement to add a 
+            scroll box in one page. Rather than just add simple 
+            text,  I decided to re-implement a previous API 
+            activity on the side as well and simply showcase 
+            the JS code for those AJAX calls. By utilizing AJAX 
+            there's also no need to refresh to do more queries.</dd>
           <dt><a href='https://github.com/omoi-sys/smallsh' target='_blank'>smallsh</a></dt>
           <dd>
             
