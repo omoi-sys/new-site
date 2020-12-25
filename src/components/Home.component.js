c77323feffcb9b6226dbe75cb6a82999e98d19d7
omoi-sys <serratoa186@gmail.com>
2020-12-24 19:27:59 -0800

    fixed broken image

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import img from '/home/omoito/new-site/src/components/milky-way-67504_1920.jpg'
 
 export default class About extends Component {
   render() {
     return (
     <div>
       <div className='welcome-cont'>
-        <img className='welcome-img' src='milky-way-67504_1920.jpg' alt='milky-way' />
+        <img className='welcome-img' src={img} alt='milky-way'/>
         <div className='welcome-head'>Welcome</div>
       </div>
       <p className='para'>
