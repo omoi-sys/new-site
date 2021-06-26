4c5ce0dac0be2c67cb77c4fcaa5ca5913369d103
omoi-sys <serratoa186@gmail.com>
2021-06-26 09:47:44 -0700

    try new path

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + 'build'));
+app.use(express.static(__dirname + 'build/index.html'));
 
 app.listen(process.env.PORT || 8080);
