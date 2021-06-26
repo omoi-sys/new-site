d6f7188f6611c767918e84399249d58382208758
omoi-sys <serratoa186@gmail.com>
2021-06-26 09:59:04 -0700

    changed a few more settings

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + 'build/index.html'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
