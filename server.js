f9ac4b117651a7a5f195a17a7e8e0ede6fab4b1f
omoi-sys <serratoa186@gmail.com>
2021-06-26 09:43:43 -0700

    changed path

--- server.js
+++ server.js
@@ -4,11 +4,4 @@ const app = express();
 
 app.use(express.static(__dirname + 'build'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
-}
-
 app.listen(process.env.PORT || 8080);
